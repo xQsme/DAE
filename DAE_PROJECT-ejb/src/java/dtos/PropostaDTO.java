64059a1be9e1f5b322e3700c30414872f35f6e4d
Gonçalo Graça Ribeiro <2151159@my.ipleiria.pt>
2018-01-02 12:35:58 +0000

    Remoção da lista de candidatos no DTO das propostas

--- DAE_PROJECT-ejb/src/java/dtos/PropostaDTO.java
+++ DAE_PROJECT-ejb/src/java/dtos/PropostaDTO.java
@@ -17,7 +17,7 @@ public class PropostaDTO  implements Serializable{
     private String tipoDeTrabalho;
     private List<String> areasCientificas;
     private String resumo;
-    private List<Student> candidatos;
+    //private List<Student> candidatos;
     private List<String> objetivos;
     private List<String> bibliografia;
     private String planoDeTrabalhos;
@@ -117,21 +117,6 @@ public class PropostaDTO  implements Serializable{
         this.estado = estado;
     }
     
-    public final void setStringEstado(String estado) {//Must be Integer because it supports null
-        switch(estado){
-            case "Não Aceite":this.estado=-1; 
-                    break;
-            case "Pendente": this.estado=0; 
-                    break;
-            case "Aceite": this.estado=1; 
-                    break;
-            case "Prova": this.estado=2;
-                    break;
-            case "Finalizado": this.estado=3; 
-                    break;
-        }
-    }
-    
     public void setObservacao(String observacao) {
         this.observacao = observacao;
     }
@@ -140,13 +125,13 @@ public class PropostaDTO  implements Serializable{
     public PropostaDTO() {
     }
 
-    public PropostaDTO(int code, String titulo, String tipoDeTrabalho, List<String> areasCientificas, String resumo, List<Student> candidatos, List<String> objetivos, List<String> bibliografia, String planoDeTrabalhos, String local, List<String> requisitos, String orcamento, String apoios, int estado, String observacao) {
+    public PropostaDTO(int code, String titulo, String tipoDeTrabalho, List<String> areasCientificas, String resumo, List<String> objetivos, List<String> bibliografia, String planoDeTrabalhos, String local, List<String> requisitos, String orcamento, String apoios, int estado, String observacao) {
         this.code = code;
         this.titulo = titulo;
         this.tipoDeTrabalho = tipoDeTrabalho;
         this.areasCientificas = areasCientificas;
         this.resumo = resumo;
-        this.candidatos = candidatos;
+        //this.candidatos = candidatos;
         this.objetivos = objetivos;
         this.bibliografia = bibliografia;
         this.planoDeTrabalhos = planoDeTrabalhos;
@@ -197,7 +182,7 @@ public class PropostaDTO  implements Serializable{
     public void setResumo(String resumo) {
         this.resumo = resumo;
     }
-
+/*
     public List<Student> getCandidatos() {
         return candidatos;
     }
@@ -205,7 +190,7 @@ public class PropostaDTO  implements Serializable{
     public void setCandidatos(List<Student> candidatos) {
         this.candidatos = candidatos;
     }
-
+*/
     public List<String> getObjetivos() {
         return objetivos;
     }
@@ -273,7 +258,7 @@ public class PropostaDTO  implements Serializable{
         setTipoDeTrabalho(null);
         setAreasCientificas(null);
         setResumo(null);
-        setCandidatos(null);
+       // setCandidatos(null);
         setObjetivos(null);
         setBibliografia(null);
         setPlanoDeTrabalhos(null);
